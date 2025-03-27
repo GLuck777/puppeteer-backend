b23433571e72f63d9ae3e9a1e58dac3888927794
jpelage <jeremy.pelage@outlook.com>
2025-03-27 11:44:30 +0100

    main/new_server

--- app/server.cjs
+++ app/server.cjs
@@ -1,28 +1,14 @@
 const express = require('express');
-const path = require('path');
 const puppeteer = require('puppeteer');
-const fs = require("fs");
 const cors = require('cors');
-const dataDir = path.join(__dirname, "../data");
-const filePath = path.join(dataDir, "users.json");
 
 const app = express();
 const PORT = 3000;
 
-// Middleware pour parser les requêtes JSON
+// Middleware
 app.use(express.json());
-
-// Activer CORS pour toutes les origines
 app.use(cors());
 
-// Servir les fichiers statiques (index.html et app.js)
-app.use(express.static(path.join(__dirname, '..'))); 
-
-app.get('/', (req, res) => {
-    // res.send('<h1>Serveur Express en ligne 🚀</h1>');
-    res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
-app.get('/favicon.ico', (req, res) => res.status(204));
 // API pour récupérer les données
 app.post('/fetch-data', async (req, res) => {
     const { username, password } = req.body;
@@ -32,10 +18,9 @@ app.post('/fetch-data', async (req, res) => {
     }
 
     try {
-        // const browser = await puppeteer.launch({ headless: true });
         const browser = await puppeteer.launch({
             args: ['--no-sandbox', '--disable-setuid-sandbox']
-          });
+        });
         const page = await browser.newPage();
         const loginUrl = "https://hub.zone01normandie.org/emargement/index.php";
 
@@ -59,29 +44,6 @@ app.post('/fetch-data', async (req, res) => {
         });
 
         await browser.close();
-        // Charger le fichier JSON s'il existe, sinon créer un tableau vide
-        // Vérifier si le dossier "data/" existe, sinon le créer
-        // if (!fs.existsSync(dataDir)) {
-        //     fs.mkdirSync(dataDir, { recursive: true });
-        // }
-
-        // Charger le fichier JSON s'il existe, sinon créer un tableau vide
-        // let users = [];
-        // if (fs.existsSync(filePath)) {
-        //     const fileData = fs.readFileSync(filePath, "utf-8");
-        //     users = JSON.parse(fileData);
-        // }
-
-        // // Vérifier si l'utilisateur existe déjà
-        // const userExists = users.some(user => user.username === username && user.password === password);
-
-        // if (!userExists) {
-        //     users.push({ username, password });
-        //     fs.writeFileSync(filePath, JSON.stringify(users, null, 4), "utf-8");
-        //     console.log("Utilisateur enregistré !");
-        // } else {
-        //     console.log("L'utilisateur existe déjà.");
-        // }
         res.json({ data: tdValue });
 
     } catch (error) {
@@ -91,5 +53,7 @@ app.post('/fetch-data', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+    console.log(`Serveur API lancé sur http://localhost:${PORT}`);
 });
+
+       
